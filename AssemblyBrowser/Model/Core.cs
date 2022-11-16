69841e68abab176a4ed8079f6c622c7a847db91e
Vyacheslav <astapovich_work@gmail.com>
2022-11-16 14:23:58 +0300

    added AssemblyBrowser and model part

---
+++ AssemblyBrowser/Model/Core.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfLibrary;
+
+namespace AssemblyBrowser.Model
+{
+    public static class Core
+    {
+        public static AssemblyInfo GetTree(string filename)
+        {
+            WpfLibrary.AssemblyBrowser assemblyBrowser = new WpfLibrary.AssemblyBrowser(filename);
+            return assemblyBrowser.GetAssemblyInfo();
+        }
+    }
+}
