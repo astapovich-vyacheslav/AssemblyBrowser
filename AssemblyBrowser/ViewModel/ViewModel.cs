3005de370aa5c91f60931717f2931f79587748d4
Vyacheslav <astapovich_work@gmail.com>
2022-11-22 15:35:49 +0300

    added ViewModel class

---
+++ AssemblyBrowser/ViewModel/ViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using AssemblyBrowser.Model;
+
+namespace AssemblyBrowser.ViewModel
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private string _assemblyPath = "";
+        private List<Node> _assemblyTree;
+        private OpenFileDialogCommand _openFileDialog;
+
+        public string AssemblyPath
+        {
+            get => _assemblyPath;
+            set
+            {
+                _assemblyPath = value;
+                AssemblyTree = AssemblyInfoToTreeConverter.Convert(Core.GetTree(AssemblyPath));
+                OnPropertyChanged(nameof(AssemblyTree));
+            }
+        }
+
+        public List<Node> AssemblyTree
+        {
+            get => _assemblyTree;
+            set => _assemblyTree = value;
+        }
+
+
+        public OpenFileDialogCommand OpenFileDialog
+        {
+            get
+            {
+                return _openFileDialog ??
+                  (_openFileDialog = new OpenFileDialogCommand(o => OpenAssembly()));
+            }
+        }
+
+        private void OpenAssembly()
+        {
+            var dialog = new Microsoft.Win32.OpenFileDialog();
+            dialog.FileName = "Assembly";
+            dialog.DefaultExt = ".dll";
+            dialog.Filter = "Dynamic link library (.dll)|*.dll";
+            dialog.Title = "Select assembly";
+
+            bool? result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                AssemblyPath = dialog.FileName;
+                OnPropertyChanged(nameof(AssemblyPath));
+            }
+        }
+    }
+}
