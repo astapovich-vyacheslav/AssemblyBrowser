731c6f24716092b5ea590122703cfdc86463a90f
Vyacheslav <astapovich_work@gmail.com>
2022-11-22 15:26:44 +0300

    OpenFileDialogCommand added

--- AssemblyBrowser/MainWindow.xaml.cs
+++ AssemblyBrowser/MainWindow.xaml.cs
@@ -24,23 +24,5 @@ namespace AssemblyBrowser
         {
             InitializeComponent(); 
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            // Configure open file dialog box
-            var dialog = new Microsoft.Win32.OpenFileDialog();
-
-            // Show open file dialog box
-            bool? result = dialog.ShowDialog();
-
-            // Process open file dialog box results
-            if (result == true)
-            {
-                // Open document
-                string filename = dialog.FileName;
-                //MessageBox.Show($"Файл открыт, имя: { filename }");
-                FileNameBox.Text = filename;
-            }
-        }
     }
 }
---
+++ AssemblyBrowser/ViewModel/AssemblyInfoToTreeConverter.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfLibrary;
+
+namespace AssemblyBrowser.ViewModel
+{
+    public static class AssemblyInfoToTreeConverter
+    {
+        public static List<Node> Convert(AssemblyInfo assemblyInfo)
+        {
+            List<Node> tree = new List<Node>();
+            foreach (var namespaceInfo in assemblyInfo.Namespaces)
+            {
+                Node node = new Node();
+                node.Header = "Namespace: " + namespaceInfo.NamespaceName;
+                node.Nodes = GetTypeNodes(namespaceInfo);
+                tree.Add(node);
+            }
+
+            return tree;
+        }
+
+        private static List<Node> GetTypeNodes(NamespaceInfo namespaceInfo)
+        {
+            List<Node> nodes = new List<Node>();
+            foreach (var typeInfo in namespaceInfo.Types)
+            {
+                Node node = new Node();
+                node.Header = "Type: " + typeInfo.Type + " " + typeInfo.TypeName;
+                node.Nodes = GetMethodNodes(typeInfo);
+                nodes.Add(node);
+            }
+
+            return nodes;
+        }
+
+        private static List<Node> GetMethodNodes(TypeInfo classInfo)
+        {
+            List<Node> nodes = new List<Node>();
+
+            foreach (var field in classInfo.Fields)
+            {
+                Node node = new Node();
+                node.Header = "Field: " + field.VarType + " " + field.VarName;
+                nodes.Add(node);
+            }
+
+            foreach (var property in classInfo.Properties)
+            {
+                Node node = new Node();
+                node.Header = "Property: " + property.VarType + " " + property.VarName;
+                nodes.Add(node);
+            }
+
+            foreach (var method in classInfo.Methods)
+            {
+                Node node = new Node();
+                node.Header = "Method: " + method.ReturnType + " " + method.MethodName;
+                node.Nodes = GetParamsNodes(method);
+                nodes.Add(node);
+            }
+
+            return nodes;
+        }
+
+        private static List<Node> GetParamsNodes(MethodInfo methodInfo)
+        {
+            List<Node> nodes = new List<Node>();
+            foreach (var param in methodInfo.Parameters)
+            {
+                Node node = new Node();
+                node.Header = "Parameter: " + param.VarType + " " + param.VarName;
+                nodes.Add(node);
+            }
+
+            return nodes;
+        }
+    }
+}
